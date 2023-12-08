1a81add197181586ea3dd3107d272cb7ded129a7
mhamad raad <hamaraad883@gmail.com>
2023-12-08 17:57:08 +0300

    add screens folder / add structure for categories screen

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,13 @@
+import { View, StyleSheet } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+import React from 'react';
+
+const CategoriesScreen = () => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default CategoriesScreen;
