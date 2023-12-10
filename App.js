3fe66d920d57a529baeaa130001292b14e1f3bb1
mhamad raad <hamaraad883@gmail.com>
2023-12-10 20:42:16 +0300

    add needed libraries / add first screen navigation

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <>
       <StatusBar style='light' />
-      <CategoriesScreen />;
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+        <CategoriesScreen />;
+      </NavigationContainer>
     </>
   );
 };
